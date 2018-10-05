0dba1430e11854a0585c8d36104c3b4c5a6d338c
Crunkagator <briantheblack@gmail.com>
2018-10-05 19:34:42 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let prost = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; 
+  let nul = [];
+  let zeroes = [];
+  for (let i = 0; i < prost.length; i++)
+  { let count = 0;
+    if(base == 1) break;
+    while (base%prost[i]==0)
+    {
+      base/=prost[i];
+      count++
+      if (base%prost[i] != 0){
+        nul.push([prost[i],count]);
+      }
+    } 
+  } 
+  for (let j = 0; j<nul.length; j++) {
+    let zc = 0;
+    let a = 1;
+    while (nul[j][0]**a <= number){
+      zc+=Math.floor(number/nul[j][0]**a++);
+    }
+    zeroes.push(Math.floor(zc/nul[j][1]));
+  }
+  zeroes.sort((a,b)=> a-b);
+  return zeroes[0];
 }
